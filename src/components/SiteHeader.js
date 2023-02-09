6b4ebcb104692cf2b61671ee62548cacf9330032
Jason Rivera <jp_rivera_@hotmail.com>
2023-02-09 11:31:37 -0800

    first commit

---
+++ src/components/SiteHeader.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const SiteHeader = () => {
+  return (
+    <div className='site-header'>
+      <Link to='/'>
+        <h1>Ninja Reviews</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default SiteHeader;
---
+++ src/pages/ReviewDetails.js
@@ -0,0 +1,9 @@
+const ReviewDetails = () => {
+  return (
+    <div>
+      <h1>Review Details</h1>
+    </div>
+  );
+};
+
+export default ReviewDetails;
